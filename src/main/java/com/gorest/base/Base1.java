e4ff6b31dff2d25ad07dd1a4801e82ea4dac8e8f
kulkarnisharda <shardakulkarni.skul@gmail.com>
2024-05-30 22:50:10 +0530

    changes

--- src/main/java/com/gorest/base/Base1.java
+++ src/main/java/com/gorest/base/Base1.java
@@ -3,7 +3,7 @@ package com.gorest.base;
 public class Base1 {
 	
 	public void baseLogic() {
-		System.out.println("I am working on Framework of Rest API");
+		System.out.println("I am working on Framework of Rest API on Eclipse workspace");
 	}
 	
 
